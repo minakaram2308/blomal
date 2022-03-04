9fa1345417c9ccd518c8e0777ccf9b5600a5b5da
minakaram2308 <minakaramcs@gmail.com>
2022-03-04 02:53:56 +0200

    firsttt

--- src/App.js
+++ src/App.js
@@ -1,3 +1,7 @@
+import '../src/assets/css/bootstrap.min.css';
+import '../src/assets/css/all.css';
+import '../src/assets/css/blomal.css';
+
 import Landing from './components/Landing'
 function App() {
   return (
--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,116 +1,84 @@
-import React from 'react'
+import React from "react";
+import logo from "../assets/img/logo.png";
+import main from "../assets/img/main.png";
+import goal1 from "../assets/img/goals/1.png";
+import goal2 from "../assets/img/goals/2.png";
+import goal3 from "../assets/img/goals/3.png";
+import goal4 from "../assets/img/goals/4.png";
+import goal5 from "../assets/img/goals/5.png";
+import time from "../assets/img/time.png";
 
 export default function Landing() {
   return (
-    <div>
-    <section className="firstSec">
-        <nav className="navbar navbar-expand-lg navbar-dark py-5">
-            <div className="container">
-                <a className="navbar-brand" href="#">
-                    <img src="img/logo.png" />
-                </a>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
-                  {/* <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li className="nav-item">
-                            <a className="nav-link active" aria-current="page" href="#">Home</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="#">Pricing</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="#">Contact</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="#">Tracking Shipment</a>
-                        </li>
-                    </ul>
-                   <div className="d-flex">
-                        <span className="nav-item rightanchor">
-                            <a className="nav-link" href="#">Products</a>
-                        </span>
-                    </div> */}
-                </div>
+    <div className="parallex">
+      {/* mainHeader */}
+      <section className="mainHeader">
+        <div class="bg-shape"></div>
+        <div className="container">
+          <nav className="navbar navbar-expand-lg navbar-dark pt-4">
+            <a className="navbar-brand" href="#">
+              <img src={logo} />
+            </a>
+          </nav>
+          <div className="row">
+            <div className="col-md-6 m-auto">
+              <h5 className="secondaryC">Smart Investing Made Easy</h5>
+              <h2 className="primaryC mb-4 fa-3x">Invest in beach life</h2>
+              <p>Start with SR 500 and build your wealth now.</p>
             </div>
-        </nav>
-        <div className="container-fluid m-auto">
-            <div className="row justify-content-center">
-                <div className="col-md-8 text-center">
-                    <p>
-                        <span className="primaryC">Data Driven</span> Software House
-                    </p>
-                    <p>
-                        Building the architecture upon which
-                    </p>
-                    <h2>Health Tech</h2>
-                    <p>
-                        can evolve
-                    </p>
-                    <img src="img/lnkr.png" />
-                </div>
+            <div className="col-md-6 position-relative">
+              <img src={main} className="w-100 position-absolute" />
             </div>
+          </div>
         </div>
-        <div className="text-center mb-5 arrowSec">
-            <a href="#lnkr" className="mt-1"><i className="fa fa-chevron-down f1p3"></i></a>
-        </div>
-    </section>
-    {/* <The_right_way */}
-    <section className="theRight text-center">
+      </section>
+      {/* <The_right_way */}
+      <section className="theRight text-center">
         <h3>The right way to invest? It's up to you.</h3>
         <p>
-            Everyone has different goals when it comes to their life, their money, and their investment plans.
+          Everyone has different goals when it comes to their life, their money,
+          and their investment plans.
         </p>
         <div className="d-flex justify-content-center">
-            <div className="goalBlock">
-                <img src="img/goals/1.png" />
-                <p>
-                    Take the trip of
-                    a lifetime
-                </p>
-            </div>
-            <div className="goalBlock">
-                <img src="img/goals/2.png" />
-                <p>
-                    Buy a home
-                </p>
-            </div>
-            <div className="goalBlock">
-                <img src="img/goals/3.png" />
-                <p>
-                    Pay for a wedding
-                </p>
-            </div>
-            <div className="goalBlock">
-                <img src="img/goals/4.png" />
-                <p>
-                    Retire early
-                </p>
-            </div>
-            <div className="goalBlock">
-                <img src="img/goals/5.png" />
-                <p>
-                    Save for a kid's
-                    education
-                </p>
-            </div>
+          <div className="goalBlock">
+            <img src={goal1} />
+            <p>Take the trip of a lifetime</p>
+          </div>
+          <div className="goalBlock">
+            <img src={goal2} />
+            <p>Buy a home</p>
+          </div>
+          <div className="goalBlock">
+            <img src={goal3} />
+            <p>Pay for a wedding</p>
+          </div>
+          <div className="goalBlock">
+            <img src={goal4} />
+            <p>Retire early</p>
+          </div>
+          <div className="goalBlock">
+            <img src={goal5} />
+            <p>Save for a kid's education</p>
+          </div>
         </div>
-    </section>
+      </section>
+      <section className="waitingApproval">
+        <div className="container m-auto">
+          <p className=" text-white">
+            Blomal Finance is a limited liability company with commercial
+            registration number 1010761056 under the procedures for obtaining a
+            permit from the Capital Market Authority.
+          </p>
+        </div>
+      </section>
 
-    {/*How It Works*/}
-    <section>
-        <div className="container">
-            <div className="row">
-                <div className="col-md-12 text-center">
-                    How It Works
-                </div>
-                <div className="col-md-3">
-
-                </div>
-            </div>
-            </div>
-    </section>
+      <footer>
+        <div className="container m-auto p-3">
+          <p className="text-center m-0">
+            Copyright © 2022 Blomal All Rights Reserved.
+          </p>
+        </div>
+      </footer>
     </div>
-  )
+  );
 }
